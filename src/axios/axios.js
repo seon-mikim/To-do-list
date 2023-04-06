e19f8380441d5dc6fd427e923c0d820053359db3
xiuxiuboK <tjsalsun@gmail.com>
2023-04-06 17:06:04 +0900

    fix: Modifying Axios instance functions

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,13 +1,18 @@
 import axios from "axios"
 
-const createInstance = axios.create({
-    baseURL: import.meta.env.VITE_SERVER_URL,
-    timeout:3000,
-    headers: {
-        'Content-Type' : 'application/json',
-        'apikey':import.meta.env.VITE_SERVER_API_KEY,
-        'username': import.meta.env.VITE_SERVER_USER_NAME
-    }
-})
+const createInstance = () => {
+   const instance = axios.create({
+        baseURL: import.meta.env.VITE_SERVER_URL,
+        timeout:3000,
+        headers: {
+            'Content-Type' : 'application/json',
+            'apikey':import.meta.env.VITE_SERVER_API_KEY,
+            'username': import.meta.env.VITE_SERVER_USER_NAME
+        }
+    
+    })
 
+    return instance
+
+}
 export const instance = createInstance()
