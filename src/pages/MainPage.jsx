922598bc149c6db32e27705667f3cf0a7ac2da57
xiuxiuboK <tjsalsun@gmail.com>
2023-04-06 17:07:41 +0900

    feat: Get data with useQuery
    - Option Chasing needs to be fixed

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import TodoModal from '../components/modal/TodoModal'
-
+import { useQuery } from 'react-query'
+import  { getTodos } from '../api/todoApi'
+import Card from '../components/common/Card'
 function MainPage() {
+  const {data: todos, isLoading, error} = useQuery('todos', getTodos)
+ console.log(todos)
   return (
     <div>
         <h1>Main Page</h1>
+        {todos?.map((todo)=> (
+
+          <Card todo={todo} key={todo.id}/>
+        )
+        )
+
+        }
         <TodoModal/>
     </div>
   )
