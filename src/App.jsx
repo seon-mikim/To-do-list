1a48858b6419d9715b47abe8602ebb5c5e024979
xiuxiuboK <tjsalsun@gmail.com>
2023-04-06 14:11:19 +0900

    feat: Creating and writing components
    -to use React query

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import  Router  from '../src/router/Router'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import {ReactQueryDevtools} from 'react-query/devtools'
 
+const queryClient = new QueryClient()
 
 function App() {
   
 
-  return <Router/>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true}/>
+      <Router/>
+    </QueryClientProvider>
+  )
 }
 
 export default App
