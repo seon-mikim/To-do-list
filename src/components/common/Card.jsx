e976c74999d37f309799d4b817f45a97cfb5e857
xiuxiuboK <tjsalsun@gmail.com>
2023-04-08 15:37:09 +0900

    feat: Create a post edit function using
    - the Update API function

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useMutation, useQueryClient, useQuery } from "react-query";
+import { getTodos, updateTodo } from "../../api/todoApi";
+import TodoInput from "../input/TodoInput";
 
-function Card({todo}) {
+function Card({ initialTodo, onDelete }) {
+  // initialTodo -> (id) todo id
+
+  const { data: todo } = useQuery("todos", getTodos, {
+    select: (todos) => todos.find((todo) => todo.id === initialTodo.id),
+  });
+
+
+  const [onEdit, setOnEdit] = useState(false);
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(updateTodo, {
+    onSuccess: (todoResponse) => {
+      queryClient.setQueriesData(
+        [`todos-${initialTodo.id}`, initialTodo.id],
+        todoResponse
+      );
+      queryClient.invalidateQueries("todos");
+
+      setOnEdit(false);
+    },
+  });
+
+  const onShowEditInput = () => {
+    setOnEdit(!onEdit);
+  };
 
   return (
-    <div key={todo.id}>
-      <div>id: {todo.id}</div>
-      <div>todo: {todo.title}</div>
+    <div key={initialTodo.id}>
+      <div>id: {initialTodo.id}</div>
+      {onEdit ? (
+        <TodoInput mutate={mutate} initialValue={todo} />
+      ) : (
+        <div>todo: {todo.title}</div>
+      )}
       <div>created: {todo.createdAt}</div>
       <div>updated: {todo.updatedAt}</div>
-      <div>{todo.done.toString()}</div>
       <div>priority: {todo.order}</div>
+      <div>
+        {onEdit ? (
+          <button onClick={onShowEditInput}>cancel</button>
+        ) : (
+          <button onClick={onShowEditInput}>edit</button>
+        )}
+
+        <button onClick={onDelete}>del</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
