e0a331a4a0867b2f9419a7be023d91ce7e8cee56
xiuxiuboK <tjsalsun@gmail.com>
2023-04-08 15:33:19 +0900

    remove: Unnecessary getTodo function

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -1,27 +1,29 @@
-import { instance } from '../axios/axios'
+import { instance } from "../../axios/axios";
 
+export const getTodos = async () => {
+  const response = await instance.get("/todos");
+  return response.data;
+};
 
-export const getTodos = async() => {
-    const response = await instance.get('/todos')
-    return response.data
-}
+/*
+    export const getTodo = async(id) => {
+        const response = await getTodos();
+        return response.find(todos => todo.id === id)
+    }
+*/
 
-export const getTodo = async(id) => {
-    const response = await instance.get(`/todos${id}`)
-    return response.data
-}
+export const createTodo = async (todo) => {
+  const response = await instance.post("/todos", todo);
+  return response.data;
+};
 
-export const createTodo = async(todo) => {
-    const response = await instance.post('/todos', todo)
-    return response.data
-}
+export const updateTodo = async ({ todo, id }) => {
+  const response = await instance.put(`/todos/${id}`, todo);
+  
+  return response.data;
+};
 
-export const updateTodo = async({todo, id}) => {
-    const response = await instance.put(`/todos${id}`, todo)
-    return response.data
-}
-
-export const deleteTodo = async({todo, id}) => {
-    const response = await instance.post(`/todos${id}`)
-    return response.data
-}
+export const deleteTodo = async (id) => {
+  const response = await instance.delete(`/todos/${id}`);
+  return response.data;
+};
