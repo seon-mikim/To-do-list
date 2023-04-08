91d98bc2d3e3d5ae2d16dc4078f3ca0a8cf1243b
xiuxiuboK <tjsalsun@gmail.com>
2023-04-08 15:46:28 +0900

    feat: Creating a function and passing
    - it mutate props

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import TodoModal from '../components/modal/TodoModal'
-import { useQuery } from 'react-query'
-import  { getTodos } from '../api/todoApi'
+import { useQuery,useQueryClient, useMutation } from 'react-query'
+import  { getTodos, deleteTodo } from '../api/todoApi'
 import Card from '../components/common/Card'
+
 function MainPage() {
+  
+  const queryClient = useQueryClient()
+  
   const {data: todos, isLoading, error} = useQuery('todos', getTodos)
+  
+  const { mutate } = useMutation(deleteTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('todos')
+    }
+  })
+
 
   return (
     <div>
         <h1>Main Page</h1>
-        {todos?.map((todo)=> (
-
-          <Card todo={todo} key={todo.id}/>
-          ))
+        { todos?.map((todo)=> 
+            (<Card initialTodo={todo} key={todo.id} onDelete={()=> mutate(todo.id)}/>))
         }
         <TodoModal/>
     </div>
