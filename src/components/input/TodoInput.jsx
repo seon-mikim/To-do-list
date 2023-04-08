4f99a799a8d12e33ebcb91ab94bdbe815c44517f
xiuxiuboK <tjsalsun@gmail.com>
2023-04-08 15:40:54 +0900

    feat: Add props and Run a function conditionally

--- src/components/input/TodoInput.jsx
+++ src/components/input/TodoInput.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
-import { useMutation } from 'react-query'
-import { createTodo } from '../../api/todoApi'
 
-function TodoInput() {
-  const { mutate, isLoading, error } = useMutation(createTodo)
 
-  const [todo, setTodo] = useState({title: ''})
+function TodoInput({mutate, initialValue={title: ''}}) {
+  const [todo, setTodo] = useState(initialValue)
   // input 일회성 redux 안쓰고 컨포넌트 hook ?? 훅 안쓰는 방법 => hook 비효율  다른 방법 찾기 => keyword 
+  const onSubmit = () => {
+  initialValue.id?  mutate({todo:todo , id:initialValue.id}): mutate(todo)
+  
+  setTodo(initialValue)
+  }
   const onChange = (e) => {
     const {name, value} = e.target
     setTodo({...todo, [name]: value})
@@ -15,7 +17,7 @@ function TodoInput() {
   return (
     <div>
         <input name="title" value={todo.title} onChange={onChange}/>
-        <button type='button' onClick={()=> mutate(todo)}>add</button>
+        <button type='button' onClick={onSubmit}>edit</button>
     </div>
   )
 }
